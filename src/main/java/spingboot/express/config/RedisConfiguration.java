f3f8e8ddd2723027f81b233c9e326051a98b155e
LiJinbao <2965251870@qq.com>
2020-11-24 11:53:30 +0800

    增加redis缓存service

--- src/main/java/spingboot/express/config/RedisConfiguration.java
+++ src/main/java/spingboot/express/config/RedisConfiguration.java
@@ -1,8 +1,102 @@
 package spingboot.express.config;
 
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.stereotype.Component;
 
-@Component
-public class RedisConfiguration {
+import java.time.Duration;
 
+@Component
+@PropertySource("application-redis.properties")
+public class RedisConfiguration {
+    @Value(value = "${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Value("${spring.redis.password}")
+    private String password;
+
+    @Value(value = "${spring.redis.timeout}")
+    private int timeout;
+
+    @Value(value = "${spring.redis.lettuce.shutdown-timeout}")
+    private long shutDownTimeout;
+
+    @Value("${spring.redis.lettuce.pool.time-between-eviction-runs}")
+     private int timeBetweenEvictionRuns;
+
+    @Value(value = "${spring.redis.lettuce.pool.max-active}")
+    private int maxActive;
+
+    @Value(value = "${spring.redis.lettuce.pool.max-wait}")
+    private int maxWait;
+
+    @Value(value = "${spring.redis.lettuce.pool.max-idle}")
+    private int maxIdle;
+
+    @Value(value = "${spring.redis.lettuce.pool.min-idle}")
+    private int minIdle;
+
+    /**
+     * 注入lettuce连接池
+     * springboot2.4.x之后，默认使用 lettuce 作为连接池的底层实现
+     * @return
+     */
+    @Bean
+    public LettuceConnectionFactory lettuceConnectionFactory() {
+        //连接池配置
+        GenericObjectPoolConfig genericObjectPoolConfig = new GenericObjectPoolConfig();
+        genericObjectPoolConfig.setMaxIdle(maxIdle);
+        genericObjectPoolConfig.setMinIdle(minIdle);
+        genericObjectPoolConfig.setMaxWaitMillis(maxWait);
+        genericObjectPoolConfig.setMaxTotal(maxActive);
+        genericObjectPoolConfig.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRuns);
+        //redis连接配置
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(host);
+        redisStandaloneConfiguration.setPort(port);
+        redisStandaloneConfiguration.setPassword(RedisPassword.of(password));
+        //连接池客户端配置
+        LettuceClientConfiguration clientConfiguration = LettucePoolingClientConfiguration.builder()
+                .commandTimeout(Duration.ofMillis(timeout))
+                .shutdownTimeout(Duration.ofMillis(shutDownTimeout))
+                .poolConfig(genericObjectPoolConfig)
+                .build();
+        LettuceConnectionFactory factory = new LettuceConnectionFactory(redisStandaloneConfiguration, clientConfiguration);
+        return factory;
+    }
+
+    @Bean
+    public RedisTemplate<String, Integer> integerRedisTemplate(LettuceConnectionFactory factory) {
+        RedisTemplate<String, Integer> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(Integer.class));
+        return template;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisCacheTemplate2Object(LettuceConnectionFactory factory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
+    }
 }
